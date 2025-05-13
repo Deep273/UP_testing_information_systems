ac5dee770ec9cbefac2b1e2afdee6a864da773df
Deep273 <soul42460@gmail.com>
2025-05-14 00:39:20 +0700

    negative4

---
+++ cypress/e2e/negative4.cy.js
@@ -0,0 +1,16 @@
+describe('Модуль Потребности – создание новой потребности работодателем', () => {
+    it('Модуль Потребности – создание новой потребности работодателем', () => {
+
+
+        cy.visit('https://dev.profteam.su/login')
+        cy.get('input[type=text]').type('testerEmployer');
+        cy.get('input[type=password]').type('Password1');
+        cy.get('button[type=submit]').eq(2).click();
+
+        cy.get(':nth-child(6) > .menu-item__item-name').should('be.visible').click();
+        cy.get('#app > div.page > div > div.page-navigation > div.page-nav__mobile > div:nth-child(7) > section > div > div.needs-block__filters-wrapper > button').click()
+
+        cy.get('body > div:nth-child(15) > div.desktop-modal > div > div.vacancy-need-wrapper > form > div:nth-child(1) > div.form__labels > div > div:nth-child(1) > div > input').type('ооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооооо')
+
+    })
+})
