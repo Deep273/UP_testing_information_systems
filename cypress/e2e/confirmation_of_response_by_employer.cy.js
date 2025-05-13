17923daa2c90562eebdc7ed52aae5e2303f6483a
Deep273 <soul42460@gmail.com>
2025-05-14 00:30:10 +0700

    negative2

--- cypress/e2e/confirmation_of_response_by_employer.cy.js
+++ cypress/e2e/confirmation_of_response_by_employer.cy.js
@@ -8,8 +8,11 @@ describe('Модуль Потребности – создание новой п
         cy.get('button[type=submit]').eq(2).click();
 
         cy.get(':nth-child(5) > .menu-item__item-name').should('be.visible').click();
-        cy.get('article:nth-child(1) .responses-list-item__actions div')
-            .first()
+        cy.get(
+            '#app > div.page > div > div.page-navigation > div.page-nav__mobile > section.responses-page > div.responses-list.responses-page__nav > div > article:nth-child(1) > button'
+        )
+            .should('be.visible')
             .click({ force: true });
+
     })
 })
